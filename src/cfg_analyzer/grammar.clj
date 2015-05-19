2c500a62c3db8cd862395e633830042c44aece91
Joe Pinsonault <joe.pinsonanult@puppetlabs.com>
2015-05-19 09:11:53 -0700

    Working on validation

--- src/cfg_analyzer/grammar.clj
+++ src/cfg_analyzer/grammar.clj
@@ -15,14 +15,25 @@
 
 (defn rules-start-with-terminals?
   "Returns true if all rules start with terminals"
-  [rules terminals]
-  (every? (fn [rule]
-            (let [[variable right-sides] rule]
-              (every? (fn
-                        [right-side]
-                        (in? (apply vector terminals) (first right-side)))
-                      right-sides)))
-          rules))
+  [rule-sets terminals]
+  (every? (fn [[variable rules]]
+            (every? (fn
+                      [rule]
+                      (in? terminals (first rule)))
+                    rules))
+          rule-sets))
+
+
+(defn rhs-dont-start-with-same-terminal?
+  "Returns true if for each variable, the rules don't start with the same terminals"
+  [rule-sets]
+  (every? (fn [[variable rules]]
+            ; If the intersection is empty, each rule started with a different terminal
+            (empty? (apply intersection
+                           (for [rule rules]
+                             ; for will return a seq of length 1 sets
+                             (set (first rule))))))
+          rule-sets))
 
 (defn validate-grammar
   "Raises an exception if the grammar has some problem"
@@ -30,7 +41,10 @@
   (if (not (variables-and-terminals-disjoint? (grammar :variables) (grammar :terminals)))
     (throw (Exception. "Variables and terminals should be disjoint")))
   (if (not (rules-start-with-terminals? (grammar :rules) (grammar :terminals)))
+    (throw (Exception. "Not all rules start with terminals")))
+  (if (not (rhs-dont-start-with-same-terminal? (grammar :rules)))
     (throw (Exception. "Not all rules start with terminals"))))
+
 
 (defn recognize?
   "Returns true if the grammar can generate the string"
