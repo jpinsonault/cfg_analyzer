7b445113735d4e4038ab60222c39707845f97904
Joe Pinsonault <joe.pinsonault@gmail.com>
2015-06-05 20:39:44 -0700

    Removed some files

--- test/cfg_analyzer/core_test.clj
+++
@@ -1,4 +0,0 @@
-(ns cfg-analyzer.core-test
-  (:require [clojure.test :refer :all]
-            [cfg-analyzer.core :refer :all]))
-
--- test/cfg_analyzer/validation_test.clj
+++
@@ -1,8 +0,0 @@
-(ns cfg-analyzer.validation-test
-  (:require [clojure.test :refer :all]
-            [cfg-analyzer.validation :refer :all]))
-
-(deftest grammars-are-valid
-  ())
-
-
